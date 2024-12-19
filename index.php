82b7dc714335cc918b4cafe02ec9663799970545
Aleksander Obrzut <EMAIL>
2024-12-19 11:25:48 +0100

    wstawianie bloków

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Plan lekcji ZSZ Bobowa</title>
+    <link rel="stylesheet" href="./CSS/styl1.css">
+    <meta http-equiv="refresh" content="300">
+</head>
+<body>
+    
+<header>
+    <img src="./zdjecia/Logo ZSZ Bobowa.png" alt="Logo ZSZ Bobowa">
+        
+    <time datetime="<?php echo date('Y-m-d\TH:i:s'); ?>">
+        <?php echo date('H:i'); ?>
+    </time>
+
+    <div class="message"></div>
+</header>
+
+<article>
+    <h2>Informacje</h2>
+</article>
+
+<aside>
+    <h2>Aktualne Lekcje</h2>
+</aside>
+    
+<nav>
+    <h2>Zdjęcia</h2>
+</nav>
+
+<main>
+    <h2>Zastępstwa</h2>
+</main>
+
+</body>
+</html>
